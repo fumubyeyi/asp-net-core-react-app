f7744073b057b678a11b938156fc40096162659a
fumubyeyi <iragaba.umubyeyi@gmail.com>
2023-06-25 15:24:42 -0400

    added CRUD operations locally

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './app/layout/style.css';
 import App from './app/layout/App';
 
@@ -7,7 +7,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
